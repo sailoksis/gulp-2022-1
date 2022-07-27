927a7e76243318abe4fbb1d1045482050847f148
sailoksis <53906219+sailoksis@users.noreply.github.com>
2022-07-28 02:43:16 +0300

    Add header and footer
    Create new dependencies html

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -8,12 +8,16 @@ const srcFolder = `./src`;
 
 export const path = {
     build: {
+        html: `${buildFolder}/`,
         files:`${buildFolder}/files/`
+        
     },
     src: {
+        html: `${srcFolder}/*.html`,
         files:`${srcFolder}/files/**/*.*`,
     },
     watch: {
+        html: `${srcFolder}/**/*.html`,
         files:`${srcFolder}/files/**/*.*`
     },
     clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,16 @@ global.app = {
 //Import tasks
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
-
+import { html } from "./gulp/tasks/html.js";
 //Watcher for file changes
 function watcher() {
     gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.html, html)
 }
+
+const mainTasks = gulp.parallel(copy, html );
 //Building scenarios for executing tasks 
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTasks, watcher);
 
 //Implementation the default script
 gulp.task('default', dev);
