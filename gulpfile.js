fdbb49277981d1dc5e542125bfc2578fa7d3db5f
sailoksis <53906219+sailoksis@users.noreply.github.com>
2022-07-28 02:04:48 +0300

    Add remove for dist
    При запуске ненужные файлы в итоговой папке дист удаляюц , если их нет в рабочей папке src

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,25 @@
-<<<<<<< HEAD
 //Main modul
 import gulp from "gulp";
 //Import paths 
 import { path } from "./gulp/config/path.js";
 
 //Send values in global variable
-=======
-import gulp from 'gulp';
-import { path } from './gulp/config/path.js';
-
-
-
->>>>>>> 379619b021c53d9f52053cbe599edae87b563b6a
 global.app = {
     path: path,
     gulp: gulp
 }
 
-<<<<<<< HEAD
 
 //Import tasks
 import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
 
 //Watcher for file changes
 function watcher() {
     gulp.watch(path.watch.files, copy)
 }
 //Building scenarios for executing tasks 
-const dev = gulp.series(copy, watcher);
+const dev = gulp.series(reset, copy, watcher);
 
 //Implementation the default script
 gulp.task('default', dev);
-=======
-import { copy } from './gulp/tasks/copy.js';
-
-gulp.task('default', copy);
->>>>>>> 379619b021c53d9f52053cbe599edae87b563b6a
