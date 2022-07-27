5270f2f3b5f4db17479188a9de6c07121a00e3e5
sailoksis <53906219+sailoksis@users.noreply.github.com>
2022-07-28 01:17:51 +0300

    Add a reset for dist

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -3,18 +3,18 @@ import * as nodePath from 'path';
 const rootFolder = nodePath.basename(nodePath.resolve());
 
 
-const buildFolder = `/.dist`; //We can change name project folder
+const buildFolder = `./.dist`; //We can change name project folder
 const srcFolder = `./src`;
 
 export const path = {
     build: {
-        files: `${buildFolder}/files/`
+        files:`${buildFolder}/files/`
     },
     src: {
-        files: `${srcFolder}/files/**/*.*`,
+        files:`${srcFolder}/files/**/*.*`,
     },
     watch: {
-        files: `${srcFolder}/files/**/*.*`
+        files:`${srcFolder}/files/**/*.*`
     },
     clean: buildFolder,
     buildFolder: buildFolder,
