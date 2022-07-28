b6ff8654e16714cf39b393937749e14adab8e136
sailoksis <53906219+sailoksis@users.noreply.github.com>
2022-07-28 06:08:24 +0300

    Notification about error in html

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,11 @@
 import replace from "gulp-replace"; //Поиск и замена        
+import plumber from "gulp-plumber"; //Обработка ошибок     
+import notify from "gulp-notify"; //Сообщения(подсказки)     
+
 
 //Экспортируем объект
 export const plugins = {
-    replace: replace
+    replace: replace,
+    plumber:plumber,
+    notify:notify,
 }
