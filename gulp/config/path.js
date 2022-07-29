fe8b0e9b4a56d05b7c296e30951c869d82ea5ae5
sailoksis <53906219+sailoksis@users.noreply.github.com>
2022-07-29 06:50:03 +0300

    Auto convert webp
    Автоматически отображает и конвертирует в webp, все кроме svg. Исправлены ошибки с background

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -11,11 +11,14 @@ export const path = {
         js: `${buildFolder}/js/`,
         css: `${buildFolder}/css/`,
         html: `${buildFolder}/`,
+        images: `${buildFolder}/img/`,
         files:`${buildFolder}/files/`
         
     },
     src: {
         js: `${srcFolder}/js/app.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
         scss: `${srcFolder}/scss/style.scss`,
         html: `${srcFolder}/*.html`,
         files:`${srcFolder}/files/**/*.*`,
@@ -24,6 +27,7 @@ export const path = {
         js: `${srcFolder}/js/**/*.js`,
         scss: `${srcFolder}/scss/**/*.scss`,
         html: `${srcFolder}/**/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
         files:`${srcFolder}/files/**/*.*`
     },
     clean: buildFolder,
