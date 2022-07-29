465d1841d3cbc479a09a7542a53d1d8894196f5e
sailoksis <53906219+sailoksis@users.noreply.github.com>
2022-07-29 06:10:20 +0300

    Add min.js
    Добавил обработку и отображение в webp, если браузер поддерживает, но пропали стили css и не отображается baclground color

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,3 +1,5 @@
+import webpack from "webpack-stream";
+
 export const js = () => {
     return app.gulp.src(app.path.src.js, {sourcemaps: true})
         .pipe(app.plugins.plumber(
@@ -6,7 +8,12 @@ export const js = () => {
                 message: "Error: <%= error.message %>"
             }))
         )
-        
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'app.min.js',
+            }
+        }))
         .pipe(app.gulp.dest(app.path.build.js))
         .pipe(app.plugins.browsersync.stream());
 }
